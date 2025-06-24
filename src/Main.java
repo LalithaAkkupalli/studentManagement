37635cdbc5b6c4276f9f3f5426fd0f1a78e715fe
Lalitha Akkupalli <114907662+LalithaAkkupalli@users.noreply.github.com>
2025-06-24 22:35:17 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+    {
+    public static void main(String[] args) 
+    {
         StudentManagement sm = new StudentManagement();
         Scanner sc = new Scanner(System.in);
-        while(true){
+        while(true)
+        {
             System.out.println("1. Add Student");
             System.out.println("2. Display Students");
             System.out.println("3. Exit");
             System.out.println("Enter your choice");
             int choice = sc.nextInt();
-            switch (choice){
+            switch (choice)
+            {
                 case 1:
                     sm.addStudent();
                     break;
